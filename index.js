5dc2cc7c28a97722561ed6444fb7f086e6d6d0c5
braxtoncuneo <braxton.s.cuneo@gmail.com>
2016-11-29 16:05:00 -0800

    Added Chat History & Foundation for Channel Selection
    Chat history is now given to all newly connected clients and persists
    over server downtime. The beginnings of channel selection is now in
    place.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+//<script>
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
+
+var record = [];
+
+var channels = {};
+
+record = require("./record.json");
+
+
+function pushNSave(theString){
+    record.push(theString);
+    fs.writeFile("record.json",JSON.stringify(record),(function(error){"do nothing";}));
+}
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -10,16 +25,35 @@ app.get('/style.css', function(req, res){
   res.sendFile(__dirname + '/style.css');
 });
 
+app.get('/wordList.json', function(req, res){
+  res.sendFile(__dirname + '/wordList.json');
+});
+
 io.on('connection', function(socket){
   console.log('a user connected');
+  var pos = 0;
+  var lim = record.length;
+  while(pos<lim){
+    io.emit('catchup', record[pos]);
+    pos++;
+  }
+  io.emit('done',"done");
+
+
   socket.on('disconnect', function(){
 	  console.log('a user disconnected');
   });
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    pushNSave(msg);
+  });
+  socket.on('login', function(msg){
+    console.log(msg);
   });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+//</script>
